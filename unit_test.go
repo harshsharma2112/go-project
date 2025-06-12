6b621216fc5075b42e42dee7844507d43f5297a7
Harsh kumar <harshkumar.h918@gmail.com>
2025-06-12 19:15:11 +0530

    Fixed UT of healthhandler

--- unit_test.go
+++ unit_test.go
@@ -4,14 +4,16 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"strings"
+
 	"testing"
 )
 
 func TestHealthHandler(t *testing.T) {
 	// 1.creating a fake http request for my func
+	t.Log("working till here")
 	req, err := http.NewRequest("GET", "/health", nil)
 	if err != nil {
-		t.Fatal("Test is failed due to %v", err) //fatal is used becoz we wanted to stop the test here only
+		t.Fatalf("Test is failed due to %v", err) //fatal is used becoz we wanted to stop the test here only
 
 	}
 	//2.recording my response
@@ -24,15 +26,15 @@ func TestHealthHandler(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
+		t.Errorf("handler returned wrong staus got %v but want %v", status, http.StatusOK)
+		//errorf to support formatting
 	}
 
-	// 5. Optionally, check if the response body contains the expected JSON
+	//5. to check the json Part
 	expected := `"status":"UP"`
 	if !strings.Contains(rr.Body.String(), expected) {
-		t.Errorf("handler returned unexpected body: got %v want to contain %v",
-			rr.Body.String(), expected)
+		t.Errorf("handler returned unexpected body: got %v want to contain %v", rr.Body.String(), expected)
 	}
-
 }
+
+
